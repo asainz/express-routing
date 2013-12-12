62bc36670a6d6ffd859abf0c4da4b085dae13ee3
Andres Sainz de Aja <andres.sainz@globant.com>
2013-12-11 21:02:58 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
+
+var read = function(filename){
+    return fs.readFileSync(filename, 'utf-8');
+};
+
+app.get(/^(.*)$/, function(req, res){
+    var uri = req.params[0];
+    
+    if( uri === '/' ){
+        res.send( read('pages/index.html') );
+        return;
+    }
+
+    //remove starting slash if there's one
+    uri = uri.charAt(0) === '/' ? uri.replace('/','') : uri;
+
+    if( fs.existsSync(uri) ){
+        res.send( read(uri) );
+        return;
+    }
+
+    res.send( read('pages/404.html') );
+});
+
+app.listen(3000);
